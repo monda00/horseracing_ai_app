9835e2cdff6a7034173392b07b3fbee2d9327921
monda <monda0524@gmail.com>
2019-11-19 22:56:01 +0900

    update render

--- view_app/views.py
+++ view_app/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
-
+from django.shortcuts import render
 from .models import Race
 
 def index(request):
     latest_race_list = Race.objects.order_by('-race_date')[:5]
-    template = loader.get_template('view_app/index.html')
-    context = {
-        'latest_race_list': latest_race_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_race_list': latest_race_list,}
+    return render(request, 'view_app/index.html', context)
+
