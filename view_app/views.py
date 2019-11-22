35990528e41fcb133e6fef0eac8304ae187a72f8
monda <monda0524@gmail.com>
2019-11-22 21:53:13 +0900

    add table html

--- view_app/views.py
+++ view_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Race
 
 def index(request):
-    latest_race_list = Race.objects.order_by('-race_date')[:5]
+    latest_race_list = Race.objects.order_by('-race_date')
     context = {'latest_race_list': latest_race_list,}
     return render(request, 'view_app/index.html', context)
 
