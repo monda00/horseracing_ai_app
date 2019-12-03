94859061f4ee69da5a5a8cc2995c3db3ec628cdd
monda <monda0524@gmail.com>
2019-12-03 22:20:41 +0900

    add predictModel app models

--- horseracing_ai_app/settings.py
+++ horseracing_ai_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'predict_model.apps.PredictModelConfig',
     'view_app.apps.ViewAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
