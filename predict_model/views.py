41e689d35a99c531bab0fa190db74c5f2e8bfbac
monda <monda0524@gmail.com>
2020-02-15 16:13:41 +0900

    fix app and model

--- predict_model/views.py
+++ predict_model/views.py
@@ -1,7 +1,1 @@
 from django.shortcuts import render
-from .models import PredictModel
-
-def result(request, race_id):
-    predict_result = "buy now!!"
-    context = {'result': predict_result,}
-    return render(request, 'predict_model/index.html', context)
