eb4fe40b6a0639b905372dfa52ae9382b99ecf08
monda <monda0524@gmail.com>
2020-02-15 16:28:14 +0900

    add races field

--- races/models.py
+++ races/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class Race(models.Model):
-    race_id = models.CharField(max_length=200)
-    race_name = models.CharField(max_length=200)
-    race_date = models.DateTimeField()
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField(null=True)
+    result  = models.CharField(max_length=200, null=True)
+    predict = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return self.race_name
+        return self.name
