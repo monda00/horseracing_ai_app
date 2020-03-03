e1f4b8fb1f33de5b73231f558e6371f643bedb90
monda <monda0524@gmail.com>
2020-03-04 00:28:37 +0900

    fix race model

--- races/views.py
+++ races/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Race
 
 def index(request):
-    latest_race_list = Race.objects.order_by('-race_date')
+    latest_race_list = Race.objects.order_by('-date')
     context = {'latest_race_list': latest_race_list,}
     return render(request, 'races/index.html', context)
 
