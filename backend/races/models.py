1735aad95cdeed5954e1cbb4ce79fabd6af76ce7
monda <monda0524@gmail.com>
2020-03-11 23:46:57 +0900

    remove predict field

--- backend/races/models.py
+++ backend/races/models.py
@@ -5,7 +5,6 @@ class Race(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField(null=True)
     result  = models.CharField(max_length=200, null=True)
-    predict = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
