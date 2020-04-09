d047de3f4e11bda5f246d1fd3e0b5708cdd16e55
monda <monda0524@gmail.com>
2020-04-10 00:11:11 +0900

    change Race Class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-function Result(props) {
-  return (
-    <button className="resutl" onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
-}
-
-class Race extends Component {
+class Races extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      races: [],
-      result: []
+      races: []
     }
   }
 
@@ -28,7 +19,6 @@ class Race extends Component {
       .then(res => {
         this.setState({
           races: res.data,
-          result: this.state.result
         });
       })
       .catch(err => {
@@ -36,45 +26,47 @@ class Race extends Component {
       })
   }
 
-  getResult(race_id) {
-    return axios
-      .post('http://localhost:8000/api/predict/', {
-        reqMsg: race_id
-      })
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-
-  handleClick(race_id) {
-    this.state.result[race_id] = this.getResult(race_id);
-  }
-
-  renderResult(race_id) {
-    return <Result
-      value={this.state.result[race_id]}
-      onClick={() => this.handleClick(race_id)}
-    />;
-  }
-
   render() {
     return (
       <div>
         {this.state.races.map(item => (
-          <div>
-            <h1>{item.name}</h1>
-            <p>{item.date}</p>
-            <p>{item.result}</p>
-            <p>{this.renderResult(item.name)}</p>
-          </div>
+            <Race value={item}/>
         ))}
       </div>
     );
   }
 }
 
-export default Race;
+class Race extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.value.name,
+      date: props.value.date
+    }
+  }
 
+  getResult(race_id) {
+    axios
+      .post('http://localhost:8000/api/predict/', {
+        reqMsg: race_id
+      })
+      .then(res => {
+        return res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.date}</p>
+      </div>
+    );
+  }
+}
+
+export default Races;
