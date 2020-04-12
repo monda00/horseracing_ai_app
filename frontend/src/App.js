448ed8fe5ffad5dbd2409627e37a5c0c500c5a34
monda <monda0524@gmail.com>
2020-04-13 00:20:59 +0900

    display result

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const beforePredictMessage = '予測します';
+
 class Races extends Component {
   constructor(props) {
     super(props);
@@ -37,12 +39,27 @@ class Races extends Component {
   }
 }
 
+function Result(props) {
+  if (props.value.result == beforePredictMessage) {
+    return (
+      <button className="predictButton" onClick={props.onClick}>
+        {props.value.result}
+      </button>
+    )
+  } else {
+    return (
+      <p>{props.value.result}</p>
+    )
+  }
+}
+
 class Race extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: props.value.name,
-      date: props.value.date
+      date: props.value.date,
+      result: beforePredictMessage
     }
   }
 
@@ -52,18 +69,37 @@ class Race extends Component {
         reqMsg: race_id
       })
       .then(res => {
-        return res.data;
+        console.log(res.data);
+        this.setState({
+          result: res.data.message
+        });
       })
       .catch(err => {
         console.log(err);
       })
   }
 
+  handleClick(race_name) {
+    if (this.state.result == beforePredictMessage) {
+      this.getResult(race_name);
+    }
+  }
+
+  renderResult(race_name) {
+    return <Result
+      value={this.state}
+      onClick={() => this.handleClick(race_name)}
+    />;
+  }
+
   render() {
     return (
-      <div>
+      <div className="race">
         <h1>{this.state.name}</h1>
         <p>{this.state.date}</p>
+        <div className="result">
+          {this.renderResult(this.state.name)}
+        </div>
       </div>
     );
   }
