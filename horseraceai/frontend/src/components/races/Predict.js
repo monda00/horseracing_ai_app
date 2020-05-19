583446d3dce4347309db8b2aa094418caae9114e
monda <monda0524@gmail.com>
2020-05-19 12:51:59 +0900

    add race component

---
+++ horseraceai/frontend/src/components/races/Predict.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Predict() {
+  return (
+    <div>
+      <p>predict result</p>
+    </div>
+  );
+}
+
+export default Predict;
---
+++ horseraceai/frontend/src/components/races/Race.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Predict } from './Predict';
+
+function Race(props) {
+  const raceInfo = props;
+  return (
+    <div>
+      <h1>{raceInfo.name}</h1>
+      <p>{raceInfo.date}</p>
+      <p>{raceInfo.result}</p>
+      <Predict />
+    </div>
+  );
+}
+
+export default Race;
