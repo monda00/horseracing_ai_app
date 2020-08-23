6153969cc940c6c7fce037657b41799ce151bc57
monda00 <monda0524@gmail.com>
2020-08-23 23:40:04 +0900

    add post api

--- horseraceai/frontend/src/components/races/Predict.js
+++ horseraceai/frontend/src/components/races/Predict.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import dummyData from '../../../../dummydata/inputdata.json';
 
-function Predict() {
+export default function Predict(props) {
+  const [predict, setPredict] = useState('predicting...');
+  const predictRace = (raceData) => {
+    const response = axios.post('/api/predict', raceData);
+    setPredict(response.data);
+  };
+  const makeJsonData = (raceData) => {
+    const data = {};
+    data.data = [raceData];
+    const jsonData = JSON.stringify(data);
+    return jsonData;
+  };
+  const readDummyData = () => {
+    const jsonData = JSON.stringify(dummyData);
+    return jsonData;
+  };
+
+  useEffect(() => {
+    predictRace(readDummyData());
+  }, []);
+
   return (
-    <>
-      <td>predict result</td>
-    </>
+    <div>
+      <td>hoge</td>
+    </div>
   );
 }
-
-export default Predict;
