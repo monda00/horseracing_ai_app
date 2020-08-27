90ed99b61ef12f037f17085e6e86369912736fce
monda00 <monda0524@gmail.com>
2020-08-28 00:06:56 +0900

    update Race Model

--- horseraceai/races/models.py
+++ horseraceai/races/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+
 class Race(models.Model):
+    # レース情報
+    race_id = models.CharField(max_length=50)
     name = models.CharField(max_length=200)
-    date = models.DateTimeField(null=True)
+    place = models.CharField(max_length=50)
+    race_horse_number = models.IntegerField()
+    distance = models.IntegerField()
+    clockwise = models.CharField(max_length=1)
+    field_type = models.CharField(max_length=1)
+    field_condition = models.CharField(max_length=1)
+    weather = models.CharField(max_length=1)
+    date_time = models.DateTimeField(null=True)
     result = models.CharField(max_length=200, null=True)
