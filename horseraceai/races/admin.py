79b6a885a6ed889f1c566784859b30eef0c6eb1b
monda00 <monda0524@gmail.com>
2020-09-08 21:58:23 +0900

    add import and export

--- horseraceai/races/admin.py
+++ horseraceai/races/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from .models import Race, Horse
 
-admin.site.register(Race)
-admin.site.register(Horse)
+
+class RaceResource(resources.ModelResource):
+    class Meta:
+        model = Race
+        skip_unchanged = True
+        report_skipped = False
+
+
+class HorseResource(resources.ModelResource):
+    class Meta:
+        model = Horse
+        skip_unchanged = True
+        report_skipped = False
+
+
+@admin.register(Race)
+class RaceAdmin(ImportExportModelAdmin):
+    resource_class = RaceResource
+
+
+@admin.register(Horse)
+class HorseAdmin(ImportExportModelAdmin):
+    resource_class = HorseResource
