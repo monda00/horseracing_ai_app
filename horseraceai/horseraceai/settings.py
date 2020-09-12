2d8c1876ec767f1446582427eb5377c0514619f3
monda00 <monda0524@gmail.com>
2020-09-12 15:37:01 +0900

    set pagination class

--- horseraceai/horseraceai/settings.py
+++ horseraceai/horseraceai/settings.py
@@ -74,6 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'horseraceai.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
