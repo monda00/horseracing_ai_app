9d5f0ef6ca63309a251f729b75baad1d6197f654
monda00 <monda0524@gmail.com>
2020-11-09 23:53:12 +0900

    update predict api encoding categorical features

--- horseraceai/predictmodel/views.py
+++ horseraceai/predictmodel/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
 
 
 @api_view(['POST'])
@@ -47,11 +48,67 @@ def set_data(data):
     input_data : ndarray
         モデルに入力できる形式の特徴量
     '''
-    input_data = np.array([list(data.get('data')[i].values())
-                           for i in range(len(data.get('data')))])
+    data = transform_categorical_data(data)
+    input_data = np.array([list(data[i].values())
+                           for i in range(len(data))])
     input_data = list(input_data.reshape((18, 1, 61)))
 
     return input_data
+
+
+def transform_categorical_data(data):
+    '''
+    カテゴリデータをLabel Encoding
+
+    Parameters
+    ----------
+    data : list
+        APIで送られてきたデータ
+        レースに出場している馬の数だけリストがある
+        それぞれの馬の特徴量は辞書型
+
+    Returns
+    -------
+    data : list
+        カテゴリデータを変換したデータ
+    '''
+    le_li = np.load(
+        BASE_DIR + '/predictmodel/label_encoding/le_list.npy', allow_pickle=True)
+    categorical_cols = create_categorical_cols()
+    data = data.get('data')
+
+    for l, c in zip(le_li, categorical_cols):
+        le = LabelEncoder()
+        le.classes_ = l.classes_
+        # 一時対応
+        if c == 'season':
+            le.classes_ = ['0', '秋', '春', '夏', '冬']
+        for i in range(len(data)):
+            data[i][c] = le.transform([str(data[i][c])])[0]
+
+    return data
+
+
+def create_categorical_cols():
+    '''
+    カテゴリデータのカラム名を生成
+
+    Returns
+    -------
+    categorical_cols : list
+        カテゴリデータのカラム名
+    '''
+    categorical_cols = ['gen', 'place', 'clockwise',
+                        'field_type', 'field_condition', 'weather', 'season']
+    categorical_cols_past_base = ['field_type', 'field_condition', 'weather']
+    past_race_num = ['one', 'two', 'three', 'four', 'five']
+    categorical_cols_past = []
+    for n in past_race_num:
+        for c in categorical_cols_past_base:
+            categorical_cols_past.append('{}_before_{}'.format(n, c))
+    categorical_cols = categorical_cols + categorical_cols_past
+
+    return categorical_cols
 
 
 def load_model():
