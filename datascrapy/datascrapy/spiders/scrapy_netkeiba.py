4fc4a8654bfceb5d8decc1afecaabedd95ed042c
monda00 <monda0524@gmail.com>
2020-12-08 00:11:28 +0900

    test scrapy to django model

--- datascrapy/datascrapy/spiders/scrapy_netkeiba.py
+++ datascrapy/datascrapy/spiders/scrapy_netkeiba.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from datascrapy.items import RaceItem
 
 
 class ScrapyNetkeibaSpider(scrapy.Spider):
     name = 'scrapy_netkeiba'
     allowed_domains = ['https://db.sp.netkeiba.com']
-    start_urls = ['http://https://db.sp.netkeiba.com/']
+    start_urls = ['https://db.sp.netkeiba.com/']
 
     def parse(self, response):
-        pass
+        item = RaceItem()
+        item['name'] = 'hoge'
+
+        yield item
