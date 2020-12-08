1591072baaf91a8c4c6268b267dd98b86a889b64
monda00 <monda0524@gmail.com>
2020-12-08 23:54:26 +0900

    scrapy to django model

--- datascrapy/datascrapy/pipelines.py
+++ datascrapy/datascrapy/pipelines.py
@@ -8,4 +8,5 @@
 
 class DatascrapyPipeline:
     def process_item(self, item, spider):
+        item.save()
         return item
--- datascrapy/datascrapy/spiders/scrapy_netkeiba.py
+++ datascrapy/datascrapy/spiders/scrapy_netkeiba.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from datascrapy.items import RaceItem
+import datetime
 
 
 class ScrapyNetkeibaSpider(scrapy.Spider):
@@ -10,6 +11,15 @@ class ScrapyNetkeibaSpider(scrapy.Spider):
 
     def parse(self, response):
         item = RaceItem()
+        item['race_id'] = '20201212'
         item['name'] = 'hoge'
+        item['place'] = 'place'
+        item['race_horse_number'] = 1
+        item['distance'] = 100
+        item['clockwise'] = 'c'
+        item['field_type'] = 'f'
+        item['field_condition'] = 'f'
+        item['weather'] = 'w'
+        item['date_time'] = datetime.datetime.now()
 
         yield item
